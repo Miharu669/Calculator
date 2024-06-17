8f5ba5c5685adbc1972a8f3408782353eb06fc9a
Miharu669 <veronicadoelfuentes@gmail.com>
2024-06-17 09:46:37 +0200

    added decimals to the calculator

--- src/views/Calculator.vue
+++ src/views/Calculator.vue
@@ -17,94 +17,21 @@
 
       <div class="key special" @click="clear()">AC</div>
       <div class="key num" @click="useNumber(0)">0</div>
+      <div class="key num" @click="addDecimal()">.</div> <!-- Nuevo botón para el punto decimal -->
       <div class="key fn" @click="divide()">/</div>
       <div class="key fn" @click="result()">=</div>
 
     </div>
   </div>
 </template>
+<style lang="scss">
 
-<script setup>
-  import { ref } from 'vue';
-
-  let equation = ref('0');
-  let resultCalled = ref(false);
-  let lastResult = ref(0);
-  /**
-   * @description This function concatenates a number to the equation
-   * @param { Number } num => number to be added to equation
-  **/
-  const useNumber = (num) => {
-    equation.value = resultCalled.value ? `${num}` : equation.value.search(/^0/g) ? `${equation.value}` + `${num}` : (equation.value.search(/^[-]$/g) !== -1 ? `${equation.value}` + `${num}` : `${num}`);
-    resultCalled.value = false;
-  };
-  const plusOperator = ' + ';
-  /**
-   * @description This function concatenates a plus sign to the equation
-  **/
-  const plus = () => {
-    equation.value = `${checkOperator(equation.value, plusOperator)}`;
-  }
-  const minusOperator = ' - ';
-  /**
-   * @description This function concatenates a minus sign to the equation
-  **/
-  const minus = () => {
-    equation.value = `${checkOperator(equation.value, minusOperator)}`;
-  }
-  const multiplyOperator = ' x ';
-  /**
-   * @description This function concatenates a multiplication sign(x) to the equation
-  **/
-  const multiply = () => {
-    equation.value = `${checkOperator(equation.value, multiplyOperator)}`;
-  }
-  const divideOperator = ' / ';
-  /**
-   * @description This function concatenates a division sign to the equation
-  **/
-  const divide = () => {
-    equation.value = `${checkOperator(equation.value, divideOperator)}`;
-  }
-  /**
-   * @description This function clears the equation
-  **/
-  const clear = () => { equation.value = '0'}
-  /**
-   * @description This function checks for an operator at the end of the equation and replaces it with the one concatenated last
-  **/
-  const checkOperator = (equation, requestedOperator) => {
-    if(equation.search(/^0$/g) !== -1){
-      if(requestedOperator.search(/( [/x] )$/g) !== -1) return '0';
-      else return requestedOperator.replace(/ /g, '')
-    }else{
-      if(resultCalled.value){
-        resultCalled.value = false;
-        return lastResult.value + requestedOperator;
-      }else{
-        return `${equation.replace(/( [+\-/x] )$/g, '')}${requestedOperator}`;
-      }
-    }
-  }
-  /**
-   * @description This function resolves the equation to give a mathematical answer
-  **/
-  const result = () => {
-    let finalEqn = equation.value.replace(/( [+\-/x] )$/g, '')
-    resultCalled.value = finalEqn.search(/( [+\-/x] )/g) !== -1
-    let eqResult = Function('"use strict";return (' + finalEqn.replace(/( \x+ )/g, ' * ') + ')')();
-    equation.value = `${eqResult.toLocaleString()}`;
-    lastResult.value = eqResult;
-  }
-</script>
-
-<style lang="scss" scoped>
 @font-face {
   font-family: "digital";
   src: local("digital"),
   url("/fonts/digital-7/digital-7.ttf");
 }
-.calc{
+.calc {
   width: 320px;
   height: 480px;
   display: flex;
@@ -112,17 +39,16 @@
   margin-left: auto;
   margin-right: auto;
   background-color: #222831;
-  border: 2px solid  #222831;
+  border: 2px solid #222831;
   top: 50%;
   left: 50%;
   translate: 0% 40%;
   border-radius: 5px;
-
 }
 
-.display{
+.display {
   flex: 1;
-  background-color: #EEEEEE;
+  background-color: #eeeeee;
   margin: 10px;
   font-size: 40px;
   text-align: right;
@@ -132,7 +58,7 @@
   border-radius: 5px;
 }
 
-.keypad{
+.keypad {
   height: 320px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
@@ -140,31 +66,96 @@
   margin: 10px;
 }
 
-.key{
+.key {
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 40px;
   cursor: pointer;
 }
-.num{
-  background-color: #393E46;
-  color: #EEEEEE;
+.num {
+  background-color: #393e46;
+  color: #eeeeee;
   border-radius: 3px;
 }
-.fn{
-  background-color: #B4B4B8;
+.fn {
+  background-color: #b4b4b8;
   color: #000000;
   border-radius: 3px;
 }
-.special{
-  background-color: #FFD369;
+.special {
+  background-color: #ffd369;
   color: #000000;
   font-size: 35px;
   font-weight: bold;
   border-radius: 3px;
 }
-::selection{
+::selection {
   background: none;
 }
+
+
 </style>
+<script setup>
+import { ref } from 'vue';
+
+let equation = ref('0');
+let resultCalled = ref(false);
+let lastResult = ref(0);
+
+const useNumber = (num) => {
+  equation.value = resultCalled.value ? `${num}` : equation.value.search(/^0/g) ? `${equation.value}` + `${num}` : (equation.value.search(/^[-]$/g) !== -1 ? `${equation.value}` + `${num}` : `${num}`);
+  resultCalled.value = false;
+};
+
+const plusOperator = ' + ';
+const plus = () => {
+  equation.value = `${checkOperator(equation.value, plusOperator)}`;
+}
+
+const minusOperator = ' - ';
+const minus = () => {
+  equation.value = `${checkOperator(equation.value, minusOperator)}`;
+}
+
+const multiplyOperator = ' x ';
+const multiply = () => {
+  equation.value = `${checkOperator(equation.value, multiplyOperator)}`;
+}
+
+const divideOperator = ' / ';
+const divide = () => {
+  equation.value = `${checkOperator(equation.value, divideOperator)}`;
+}
+
+const clear = () => { equation.value = '0' }
+
+const checkOperator = (equation, requestedOperator) => {
+  if(equation.search(/^0$/g) !== -1){
+    if(requestedOperator.search(/( [/x] )$/g) !== -1) return '0';
+    else return requestedOperator.replace(/ /g, '')
+  }else{
+    if(resultCalled.value){
+      resultCalled.value = false;
+      return lastResult.value + requestedOperator;
+    }else{
+      return `${equation.replace(/( [+\-/x] )$/g, '')}${requestedOperator}`;
+    }
+  }
+}
+
+const result = () => {
+  let finalEqn = equation.value.replace(/( [+\-/x] )$/g, '')
+  resultCalled.value = finalEqn.search(/( [+\-/x] )/g) !== -1
+  let eqResult = Function('"use strict";return (' + finalEqn.replace(/( \x+ )/g, ' * ') + ')')();
+  equation.value = `${eqResult.toLocaleString()}`;
+  lastResult.value = eqResult;
+}
+
+const addDecimal = () => {
+  // Evitar múltiples puntos decimales en el mismo número
+  if (!equation.value.includes('.') || /[\+\-x\/] \d*$/.test(equation.value)) {
+    equation.value += '.';
+  }
+}
+</script>
